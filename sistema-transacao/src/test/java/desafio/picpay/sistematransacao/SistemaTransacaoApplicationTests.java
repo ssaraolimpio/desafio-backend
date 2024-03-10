71ac38e610fee4a20c351e91d124fc230056c745
SaraOlimpio <s.olimpio8@outlook.com>
2024-03-10 20:46:44 -0300

    criacao do projeto

---
+++ sistema-transacao/src/test/java/desafio/picpay/sistematransacao/SistemaTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.picpay.sistematransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
